2e9bb81e011608134e4bdc007917bb0b394f8d00
Christian Vizcardo <christianvizcardo@MacBook-Pro-de-Christian.local>
2025-10-25 08:14:28 -0500

    Feature filtro de fecha Bumeran implementado y funcionando

--- src/bumeran.py
+++ src/bumeran.py
@@ -7,14 +7,25 @@ class BumeranScraper:
     def __init__(self):
         self.driver = webdriver.Firefox()
 
-    def abrir_pagina_empleos(self):
-        self.driver.get("https://www.bumeran.com.pe/empleos-busqueda.html")
+    def abrir_pagina_empleos(self, hoy: bool = False, dias: int = 0):
+        
+        # Mejorable y añadible mas condiciones
+        if hoy:
+            self.driver.get("https://www.bumeran.com.pe/empleos-publicacion-hoy.html")
+        elif not hoy:
+            if dias == 2:
+                self.driver.get("https://www.bumeran.com.pe/empleos-publicacion-menor-a-2-dias.html")
+            elif dias == 3:
+                self.driver.get("https://www.bumeran.com.pe/empleos-publicacion-menor-a-3-dias.html")
+            else:
+                self.driver.get("https://www.bumeran.com.pe/empleos-busqueda.html")
 
     # Errores aqui
-    def escribir_vacante(self):
+    def buscar_vacante(self, palabra_clave: str = ''):
+        # El id del box de texto
         placeholder_name = "react-select-4-input"
         elem = self.driver.find_element(By.ID, placeholder_name)
-        elem.send_keys("TESTING")
+        elem.send_keys(palabra_clave)
         elem.send_keys(Keys.RETURN)
 
     def close(self):
@@ -22,7 +33,7 @@ class BumeranScraper:
 
 if __name__ == "__main__":
     scraper = BumeranScraper()
-    scraper.abrir_pagina_empleos()
-    scraper.escribir_vacante()  
+    scraper.abrir_pagina_empleos(dias=2)
+    scraper.buscar_vacante('Data Scientist')  
     # Add more scraping logic here
     #scraper.close()
